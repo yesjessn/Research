fe90f1d5694b4289ca90408f4fbc096867acf38d
yesjessn <yelnguyen@ucdavis.edu>
2016-02-08 15:20:17 -0800

    Load data file

--- Exp2.R
+++ Exp2.R
@@ -11,3 +11,7 @@ library(lmerTest)
 
 # Import fonts
 loadfonts(device="win")
+
+# Load trial report and fix NA errors
+setwd("C:/Users/Jessica/Documents/Research/data/data_exp2")
+df_exp2 <- read.delim('trial_report_282016_fixed.txt', na.strings = c(" ", ".", "NA", ""))
