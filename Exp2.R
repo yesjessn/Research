bda59f4d610779cc52571e0bf6b42472cc439273
yesjessn <yelnguyen@ucdavis.edu>
2016-02-08 15:15:56 -0800

    Load Fonts

---
+++ Exp2.R
@@ -0,0 +1,13 @@
+# Packages (may not be using all)
+library(broom)
+library(dplyr)
+library(extrafont)
+library(ggplot2)
+library(grid)
+library(lme4)
+library(tidyr)
+library(zoo)
+library(lmerTest)
+
+# Import fonts
+loadfonts(device="win")
